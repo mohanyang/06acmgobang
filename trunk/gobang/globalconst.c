f65ba8897d0b8ff940c18807f1cf630cc68135bb
cxreinstein <cxreinstein@dc6b22dc-45d4-11de-84a6-839b6ed6c72e>
2009-05-29 13:00:47 +0000

    conf parser

--- trunk/gobang/globalconst.c
+++ trunk/gobang/globalconst.c
@@ -4,7 +4,25 @@
 #include <stdlib.h>
 
 void initializeConstant(){
-	FILE *fp=fopen("gobang.conf", "r");
-	// TODO
-	fclose(fp);
+  FILE *fp=fopen("gobang.conf", "r");
+  
+  char line[200];
+  while (fgets(line, 200, fp) != NULL) {
+    char *first;
+    for (first = line; *first == ' ' || *first == '\t'; ++first);
+    if (*first == 0 || *first == '\n' || *first == '#')
+      continue;
+    char *key, *value;
+    key = strtok(line, " \t");
+    value = strtok(NULL, " \t");
+    value = strtok(NULL, " \t");
+    if (strcmp(key, "timelimit") == 0) {
+      const_tls = atoi(value) / 1000;
+      const_tlm = atoi(value) % 1000;
+    } else if (strcmp(key, "forbidden") == 0) {
+      const_forbidden = strcmp(value, "true") == 0;
+    }
+  }
+  
+  fclose(fp);
 }
